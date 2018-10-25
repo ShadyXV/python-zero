8380e6f4f46e3fba9cad5aacc9f8906c7e85e9a1
Aashish Karki <aashish.y2z@gmail.com>
2018-10-25 12:04:55 +0545

    Add score

--- space-invaders/space-invader.py
+++ space-invaders/space-invader.py
@@ -21,7 +21,18 @@ for side in range(4) :
   border_pen.lt(90)
 border_pen.hideturtle()
 
+##Score and initiate to 0
+score = 0
 
+#Draw the score
+score_pen = turtle.Turtle()
+score_pen.speed(0)
+score_pen.color("white")
+score_pen.penup()
+score_pen.setposition(-290, 280)
+scorestring = "Score: %s" %score
+score_pen.write(scorestring, False, align="left", font=("Arial", 14, "normal"))
+score_pen.hideturtle()
 ## Choose number of enemies
 number_of_enemies = 5
 
@@ -150,6 +161,12 @@ while True:
       x = random.randint(-200, 200)
       y = random.randint(100, 250)
       enemy.setposition(x, y)
+      #Update the score
+      score += 10
+      scorestring = "Score: %s" %score
+      score_pen.clear()
+      score_pen.write(scorestring, False, align="left", font=("Arial", 14, "normal"))
+
 
     if isCollision(player, enemy) :
       player.hideturtle()

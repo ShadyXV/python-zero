abac57d70710b1d2cc2e47ca9418380e75b6c901
Aashish Karki <aashish.y2z@gmail.com>
2018-11-22 15:03:35 +0545

    Complete getRepo

---
+++ github/getRepo/README.md
@@ -0,0 +1,8 @@
+## command
+```python
+python main.py <githubUserName>
+```
+
+## output
+
+<img width="659" alt="screen shot 2018-11-22 at 3 01 06 pm" src="https://user-images.githubusercontent.com/12614476/48893032-79f5e380-ee67-11e8-9340-66a5718305a4.png">
--- github/getRepo/main.py
+++ github/getRepo/main.py
@@ -1,16 +1,47 @@
 from _helper import simple_get
 from bs4 import BeautifulSoup
 import sys
+import json
+
+repoName = []
+repoLink = []
+
+
+
+def populateData(url):
+  raw_html = simple_get(url)
+  html = BeautifulSoup(raw_html, 'html.parser')
+  repos = html.find_all("a", {"itemprop": "name codeRepository"})
+
+  for repo in repos:
+    repoName.append(repo.text)
+    repoLink.append('http://github.com' + repo["href"])
+
+  pagination = html.find_all("div", {"class": "pagination"})
+  next = html.find_all("a", {"rel": "nofollow"})
+
+  nextUrl = None
+
+  if len(pagination) == 1 :
+    if next[1].text == 'Next' :
+      nextUrl = next[1]["href"]
+
+  return nextUrl
 
 try:
   userName= sys.argv[1]
   url = "http://github.com/"+userName+"?tab=repositories"
-  raw_html = simple_get(url)
-  html = BeautifulSoup(raw_html, 'html.parser')
-  div = html.select("div#user-repositories-list")
-  anotherDiv = BeautifulSoup(div, "html.parser")
+  while True:
+    nextUrl = populateData(url)
+    if nextUrl == None:
+      break
+    else:
+      url = nextUrl
 
 except Exception:
-  print ('Error')
+  print ('Command : python main.py <gitHub username>')
 
-# check if the
+
+for i in range(0, len(repoName)) :
+  print(repoName[i])
+  print(repoLink[i])

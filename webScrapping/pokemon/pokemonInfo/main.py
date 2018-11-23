4d2dd8a925faeeebc6b4345d97512b88e8ffa44f
Aashish Karki <aashish.y2z@gmail.com>
2018-11-23 14:59:50 +0545

    Finish basic implementation

--- webScrapping/pokemon/pokemonInfo/main.py
+++ webScrapping/pokemon/pokemonInfo/main.py
@@ -13,4 +13,11 @@ url = str(base_url+ list[0].strip())
 
 raw_html = simple_get(url)
 html = BeautifulSoup(raw_html, 'html.parser')
-print(raw_html)
+## Stat Table
+stat_table_number = 128
+table = html.select('table')[stat_table_number]
+tr = table.select('table')
+for value in range(0, 6):
+  name = tr[value].select('th')[0].select('a')[0].text
+  value = tr[value].select('th')[1].text
+  print(value)

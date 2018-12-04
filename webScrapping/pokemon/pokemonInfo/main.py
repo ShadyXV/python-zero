bd6d1811036bf3b256bca3fb06f071baa0759e12
Aashish Karki <aashish.y2z@gmail.com>
2018-12-04 15:04:14 +0545

    Get pokemon info

--- webScrapping/pokemon/pokemonInfo/main.py
+++ webScrapping/pokemon/pokemonInfo/main.py
@@ -1,38 +1,38 @@
 import os
 from _helper import simple_get, json_helper
 from bs4 import BeautifulSoup
-import difflib
 
-pokemon_list = open('../pokemonList/pokemonlist.txt', 'r')
+
+## open the file to write
+
 
 f = open("pokemon.json", "a")
 
 f.write("{\n")
 
-list = pokemon_list.readlines()
-base_url = 'https://bulbapedia.bulbagarden.net'
-url = str(base_url+ list[0].strip())
 
+url = 'https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_base_stats_(Generation_I)'
 raw_html = simple_get(url)
 html = BeautifulSoup(raw_html, 'html.parser')
-## Stat Table
-stat_table_number = 128
-name_pokemon = html.select('h1#firstHeading')[0].text.split(' ', 1)[0]
 
-f.write(json_helper(0))
-f.write("\t")
-f.write(json_helper("name", name_pokemon)+ ", \n")
-f.write("\t")
-table = html.select('table')[stat_table_number]
-tr = table.select('table')
-f.write(json_helper("stats"))
+required_table = html.select('table')[0]
+rows = required_table.select('tr')
+for individual in range(1, len(rows)):
+  f.write('\t' + json_helper(individual))
+  row = rows[individual]
+  column = row.select("td")
+  evolutionid = individual + 1
+  f.write('\t\t'+ json_helper('Name', column[2].text.strip()) + ", \n")
+  f.write('\t\t'+ json_helper('HP', column[3].text.strip()) + ", \n")
+  f.write('\t\t'+ json_helper('Attack', column[4].text.strip()) + ", \n")
+  f.write('\t\t'+ json_helper('Defense', column[5].text.strip()) + ", \n")
+  f.write('\t\t'+ json_helper('Speed', column[6].text.strip()) + ", \n")
+  f.write('\t\t'+ json_helper('Special', column[7].text.strip()) + ", \n")
+  f.write('\t\t'+ json_helper('Level', '15') + ", \n")
+  f.write('\t\t'+ json_helper('EvolutionId', str(evolutionid)) + ", \n")
+  f.write('\t\t'+ json_helper('Type', "[]") + ", \n")
+  f.write("\t },\n")
 
-for value in range(0, 6):
-  name = tr[value].select('th')[0].select('a')[0].text.rstrip()
-  value = tr[value].select('th')[1].text.rstrip()
-  f.write("\t\t")
-  f.write(json_helper(name, value.strip())+", \n")
 
-f.write("\t\t}\n")
-f.write("\t}")
-f.write("\n}")
+f.write("}")
+f.close()

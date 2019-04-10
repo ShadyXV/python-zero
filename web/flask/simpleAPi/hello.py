93a6162f7ef44a4be112881093a9e33fc726bf59
Aashish Karki <aashish.y2z@gmail.com>
2019-04-10 15:12:21 +0545

    add a so,[le getter function

--- web/flask/simpleAPi/hello.py
+++ web/flask/simpleAPi/hello.py
@@ -1,6 +1,26 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, url_for
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+app = Flask("__main__")
+app.config['TESTING'] = True
+
+
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(username)
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
+
+if __name__ == '__main__':
+    app.run(debug=True)

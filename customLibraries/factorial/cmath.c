ff6718015b4a1b430751040fb31577ffb4b4699f
Aashish Karki <aashish.y2z@gmail.com>
2019-04-10 14:27:51 +0545

    add python with c support

---
+++ customLibraries/factorial/cmath.c
@@ -0,0 +1,16 @@
+#include <Python.h>
+
+int main()
+{
+	PyObject* pInt;
+
+	Py_Initialize();
+
+	PyRun_SimpleString("print('Hello World from Embedded Python!!!')");
+
+	Py_Finalize();
+
+	printf("\nPress any key to exit...\n");
+	if(!_getch()) _getch();
+	return 0;
+}
